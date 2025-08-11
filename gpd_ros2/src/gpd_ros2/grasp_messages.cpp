73047988e5cc2c1d732453e98992265526d5ca2f
Rohit Menon <rohit.menon@uni-bonn.de>
2025-08-11 17:41:58 +0000

    added grasp messages

--- gpd_ros2/src/gpd_ros2/grasp_messages.cpp
+++ gpd_ros2/src/gpd_ros2/grasp_messages.cpp
@@ -26,3 +26,29 @@ gpd_ros2_msgs::msg::GraspConfig GraspMessages::convertToGraspMsg(const gpd::cand
 
   return msg;
 }
+
+#include <Eigen/Geometry>
+#include "gpd_ros2_msgs/msg/grasp_params.hpp"
+
+gpd::DetectParams convertGraspParamsToDetectParams(const gpd_ros2_msgs::msg::GraspParams &m) {
+  gpd::DetectParams d;
+  d.approach_direction << m.approach_direction.x, m.approach_direction.y, m.approach_direction.z;
+
+  const size_t n = m.camera_position.size();
+  const size_t cols = n / 3;
+  if (n % 3 == 0 && n > 0) {
+    d.camera_position = Eigen::Map<const Eigen::Matrix<double,3,Eigen::Dynamic>>(m.camera_position.data(), 3, cols);
+  } else {
+    d.camera_position.resize(3, 0);
+  }
+
+  const auto &t = m.transform_camera2base.translation;
+  const auto &r = m.transform_camera2base.rotation;
+  d.transform_camera2base = Eigen::Translation3d(t.x, t.y, t.z) * Eigen::Quaterniond(r.w, r.x, r.y, r.z);
+
+  d.workspace = m.workspace;
+  d.can_filter_approach = m.enable_approach_dir_filtering;
+  d.thresh_rad = m.approach_dir_threshold;
+  return d;
+}
+
